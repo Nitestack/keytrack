a52c0b2eac95234613c0dde19e64f4910213e336
Nitestack <code@npham.de>
2025-08-31 02:21:01 +0200

    feat: don't disable controls for metronome while playing

--- src/app/(dashboard)/repertoire/[musicBrainzId]/pdf-viewer/metronome/bpm.tsx
+++ src/app/(dashboard)/repertoire/[musicBrainzId]/pdf-viewer/metronome/bpm.tsx
@@ -20,7 +20,6 @@ const MetronomeBpm: FC = () => {
   const setBpm = useMetronomeStore((state) => state.setBpm);
   const increaseBpm = useMetronomeStore((state) => state.increaseBpm);
   const decreaseBpm = useMetronomeStore((state) => state.decreaseBpm);
-  const isPlaying = useMetronomeStore((state) => state.isPlaying);
   const isIncreasingBpmDisabled = useMetronomeStore(
     (state) => state.isIncreasingBpmDisabled,
   );
@@ -33,23 +32,21 @@ const MetronomeBpm: FC = () => {
       <Typography className="font-bold select-none" variant="h4">
         {bpm} <Typography variant="caption">BPM</Typography>
       </Typography>
-      {!isPlaying && (
-        <div className="flex items-center w-full gap-4">
-          <IconButton disabled={isDecreasingBpmDisabled} onClick={decreaseBpm}>
-            <RemoveIcon />
-          </IconButton>
-          <Slider
-            aria-label="bpm"
-            value={bpm}
-            onChange={(_, newBpm) => setBpm(newBpm)}
-            min={minBpm}
-            max={maxBpm}
-          />
-          <IconButton disabled={isIncreasingBpmDisabled} onClick={increaseBpm}>
-            <AddIcon />
-          </IconButton>
-        </div>
-      )}
+      <div className="flex items-center w-full gap-4">
+        <IconButton disabled={isDecreasingBpmDisabled} onClick={decreaseBpm}>
+          <RemoveIcon />
+        </IconButton>
+        <Slider
+          aria-label="bpm"
+          value={bpm}
+          onChange={(_, newBpm) => setBpm(newBpm)}
+          min={minBpm}
+          max={maxBpm}
+        />
+        <IconButton disabled={isIncreasingBpmDisabled} onClick={increaseBpm}>
+          <AddIcon />
+        </IconButton>
+      </div>
     </>
   );
 };
