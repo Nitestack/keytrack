cfed36f5a6c45c0b3495dd003ceb6f43a9c13114
Nitestack <code@npham.de>
2025-12-30 23:17:21 +0100

    feat: replace Embla Carousel with yarl

---
+++ src/app/(dashboard)/repertoire/[musicBrainzId]/score-viewer/lightbox.tsx
@@ -0,0 +1,25 @@
+import LightboxComponent from "yet-another-react-lightbox";
+import Captions from "yet-another-react-lightbox/plugins/captions";
+import Download from "yet-another-react-lightbox/plugins/download";
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/captions.css";
+import "yet-another-react-lightbox/plugins/counter.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+
+import type { FC } from "react";
+import type { LightboxExternalProps } from "yet-another-react-lightbox";
+
+const Lightbox: FC<Omit<LightboxExternalProps, "plugins">> = (props) => {
+  return (
+    <LightboxComponent
+      plugins={[Captions, Download, Fullscreen, /*Thumbnails,*/ Zoom]}
+      {...props}
+    />
+  );
+};
+
+export default Lightbox;
