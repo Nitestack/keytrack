8d187d57e7af054230c82f95a484dac83bb91cf5
Nitestack <code@npham.de>
2026-01-01 22:13:51 +0100

    feat: don't integrate zoom

--- src/app/(dashboard)/repertoire/[musicBrainzId]/score-viewer/lightbox.tsx
+++ src/app/(dashboard)/repertoire/[musicBrainzId]/score-viewer/lightbox.tsx
@@ -1,12 +1,9 @@
 import LightboxComponent from "yet-another-react-lightbox";
-import Captions from "yet-another-react-lightbox/plugins/captions";
 import Download from "yet-another-react-lightbox/plugins/download";
 import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
 import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
-import Zoom from "yet-another-react-lightbox/plugins/zoom";
 
 import "yet-another-react-lightbox/styles.css";
-import "yet-another-react-lightbox/plugins/captions.css";
 import "yet-another-react-lightbox/plugins/counter.css";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 
@@ -16,7 +13,7 @@ import type { LightboxExternalProps } from "yet-another-react-lightbox";
 const Lightbox: FC<Omit<LightboxExternalProps, "plugins">> = (props) => {
   return (
     <LightboxComponent
-      plugins={[Captions, Download, Fullscreen, Thumbnails, Zoom]}
+      plugins={[Download, Fullscreen, Thumbnails]}
       {...props}
     />
   );
